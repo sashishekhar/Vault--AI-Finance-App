770381c1b2e17d4952a660c0d5f56e1c25cc8db5
Sashi Sekhar Singh <118200469+sashishekhar@users.noreply.github.com>
2025-04-22 16:04:20 +0530

    Update Hero.jsx

--- components/Hero.jsx
+++ components/Hero.jsx
@@ -42,9 +42,9 @@ const HeroSection = () => {
               Get Started
             </Button>
           </Link>
-          <Link href="https://www.youtube.com/roadsidecoder">
+          <Link href="https://github.com/sashishekhar/Vault--AI-Finance-App">
             <Button size="lg" variant="outline" className="px-8">
-              Watch Demo
+              ReadMe.md
             </Button>
           </Link>
         </div>
